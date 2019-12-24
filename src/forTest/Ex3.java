f5ecf309c5b59ce64c0674f47ba61bae01d0f10c
Flexnm <58367828+Flexnm@users.noreply.github.com>
2019-12-24 13:59:21 +0200

    Update Ex3.java

--- src/forTest/Ex3.java
+++ src/forTest/Ex3.java
@@ -5,29 +5,29 @@ import java.util.Scanner;
 public class Ex3 {
 	public static void main(String[] args) {
 
-		int num = (int) (Math.random() * 11);
+		int num = (int) (Math.random() * 11); // pick a random number (in this case between 0 to 10).
 
-		Scanner scan = new Scanner(System.in);
-		System.out.println(num);
+		Scanner scan = new Scanner(System.in); // start a scanner object to recieve keyboard input.
+		// System.out.println(num); // print the number for myself to test the program.
 
-		System.out.println("A random number was introduced!");
-		System.out.println("Write down your guess: ");
+		System.out.println("A random number was introduced!"); // print out scenario
+		System.out.println("Write down your guess: "); // print instructions.
 
 		
-		int i;
-		for (i = 1; i <= 3; i++) {
-			int guess = scan.nextInt();
-			if (guess != num) {
-				System.out.println("You have entered the wrong number, guess again: ");
+		int i; // initialize 'i' outside the 'for' loop so it could be used outside of it.
+		for (i = 1; i <= 3; i++) { // run the loop exactly 3 times.
+			int guess = scan.nextInt(); // the input is taken in the loop itself.
+			if (guess != num) { // if user guesses wrong.
+				System.out.println("You have entered the wrong number, guess again: "); // print to retry.
 				
-			} else {
-				System.out.println("You did it!");
-				break;
+			} else { // if user guesses right.
+				System.out.println("You did it!"); // print success.
+				break; // and exit the for loop.
 			}
 		}
-		if(i == 4) {
-			System.out.println("Too bad...");
+		if(i == 4) { // if the user failed 3 times (aka, 'i > 3'.
+			System.out.println("Too bad..."); // print failiure.
 		}
-		scan.close();
+		scan.close(); // end the scanner operation whether there was success or failiure.
 	}
 }
