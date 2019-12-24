3bacfea28c847116da013c3c730819a3c8e76cc0
Flexnm <58367828+Flexnm@users.noreply.github.com>
2019-12-24 13:50:23 +0200

    Update Ex1.java

--- src/forTest/Ex1.java
+++ src/forTest/Ex1.java
@@ -7,39 +7,38 @@ public class Ex1 {
 	public static void main(String[] args) {
 		
 		int[] intArr = new int[5];
-		//Random rand = new Random();
 		double avg = 0;
 		
-		int index = 0;
-		int five = 0;
-		int max = 0;
-		int min = 10;
+		int index = 0; // Index of the element checked.
+		int five = 0; // Number of elements that are larger than 5.
+		int max = 0; // The maximum value in the array.
+		int min = 10; // The minimum value in the array. Set to the largest possible value to be put in the array.
 		
 		while(index < intArr.length) {
-			int x = (int)(Math.random()*10 + 1);
-			int i;
+			int x = (int)(Math.random()*10 + 1); // randomize a number within range.
+			int i; 
 			for (i = 0; i < intArr.length; i++) {
-				if(intArr[i] == x) {
+				if(intArr[i] == x) { // if there is a dupliacete, dont add it to the array.
 					break;
 				}
 			}
 			
-			if(i == intArr.length) {
-				intArr[index] = x;
-				index++;
-				avg += x;
-				if(x > 5) {
-					five++;
+			if(i == intArr.length) { // if its not a duplicate (if we didnt break):
+				intArr[index] = x; // add it to the array.
+				index++; // prepare to check the next element in the array.
+				avg += x; // add the number to the sum of all elements of the array.
+				if(x > 5) { // check if it is larger than 5.
+					five++; // if it is count it as a number that is larger than 5.
 				}
-				for (int j = 0; j < intArr.length; j++) {
-					if(x > max) {
-						max = x;
-					} else if(x < min) {
-						min = x;
+				for (int j = 0; j < intArr.length; j++) { // checking max and min.
+					if(x > max) { // if that number is larger than max:
+						max = x; // make it max.
+					} else if(x < min) { // if the number is smaller than min:
+						min = x; // make it min.
 					}
 				}
 			}
-		}
+		} // print everything.
 		System.out.println(Arrays.toString(intArr));
 		System.out.println(max);
 		System.out.println(min);
